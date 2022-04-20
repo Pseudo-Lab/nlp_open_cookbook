c1afeabb0037655372478a19457d20573518cbe5
junuMoon <francomoon7@gmail.com>
2022-04-20 13:37:41 +0900

    Test Passed: LogisticRegression with BoW accuracy: 0.766

--- classification/model/Tree/tests/conftest.py
+++ classification/model/Tree/tests/conftest.py
@@ -6,7 +6,17 @@ path = Path(__file__)
 data_dir = path.absolute().parent.parent.parent.parent / 'data'
 
 
-@pytest.fixture
+@pytest.fixture(scope='session')
 def bn_test_data():
     return pd.read_csv(data_dir / 'test_binary.csv')
 
+@pytest.fixture(scope='session')
+def bn_train_data():
+    return pd.read_csv(data_dir / 'train_binary.csv')
+
+@pytest.fixture(scope='session')
+def accuracy_dict():
+    return {
+        'base_model': 0.5,
+        'logistic_regression': 0.5,
+    }
--- classification/model/Tree/tests/test_base_model.py
+++ classification/model/Tree/tests/test_base_model.py
@@ -1,5 +1,8 @@
 import pytest
 
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.linear_model import LogisticRegression
+
 class BaseModel:
     def __init__(self):
         pass
@@ -7,11 +10,44 @@ class BaseModel:
     def predict(self, data):
         return '긍정'
 
-def test_base_model(bn_test_data):
-    test_x = bn_test_data['text']
-    test_y = bn_test_data['label']
+class LogisticRegressionModel(BaseModel):
+    def __init__(self):
+        self.model = LogisticRegression()
 
-    base_model = BaseModel()
-    predict = list(map(base_model.predict, test_x))
-    accuracy = sum(predict == test_y) / len(test_y)
-    assert accuracy >= 0.5
+    def train(self, X, y):
+        self.model.fit(X, y)
+
+    def predict(self, X):
+        return ['긍정' if pd else '부정' for pd in self.model.predict(X) if pd]
+
+
+class TestBaseModel:
+    
+    def test_base_model(self, bn_test_data, accuracy_dict):
+        test_x = bn_test_data['text']
+        test_y = bn_test_data['label']
+
+        base_model = BaseModel()
+        predict = list(map(base_model.predict, test_x))
+        accuracy = sum(predict == test_y) / len(test_y)
+        accuracy_dict['base_model'] = accuracy
+        print("BaseModel accuracy:", accuracy)
+        assert accuracy >= 0.5
+
+    def test_logistic_regressino(self, bn_train_data, bn_test_data, accuracy_dict):
+        train_x = bn_train_data['text']
+        train_y = bn_train_data['label']
+
+        test_x = bn_test_data['text']
+        test_y = bn_test_data['label']
+
+        bow = CountVectorizer()
+        train_x_emb = bow.fit_transform(train_x)
+        test_x_emb = bow.transform(test_x)
+
+        lr = LogisticRegression()
+        lr.fit(train_x_emb, train_y)
+        predict = lr.predict(test_x_emb)
+        accuracy = sum(predict == test_y) / len(test_y)
+        print("LogisticRegression accuracy:", accuracy) 
+        assert accuracy >= accuracy_dict['base_model']  # 0.7665
