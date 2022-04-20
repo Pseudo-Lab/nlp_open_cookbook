39022581f02db9ff96ecf4f9510e5dc515f1d7be
junuMoon <francomoon7@gmail.com>
2022-04-20 19:07:21 +0900

    Test code refactoring

--- classification/model/Tree/tests/conftest.py
+++ classification/model/Tree/tests/conftest.py
@@ -10,9 +10,21 @@ data_dir = path.absolute().parent.parent.parent.parent / 'data'
 def bn_test_data():
     return pd.read_csv(data_dir / 'test_binary.csv')
 
+def train_x(bn_train_data):
+    return bn_train_data['text']
+
+def train_y(bn_train_data):
+    return bn_train_data['label']
+
 @pytest.fixture(scope='session')
 def bn_train_data():
     return pd.read_csv(data_dir / 'train_binary.csv')
+
+def test_x(bn_test_data):
+    return bn_test_data['text']
+
+def test_y(bn_test_data):
+    return bn_test_data['label']
 
 @pytest.fixture(scope='session')
 def accuracy_dict():
--- classification/model/Tree/tests/test_base_model.py
+++ classification/model/Tree/tests/test_base_model.py
@@ -3,6 +3,7 @@ import pytest
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.linear_model import LogisticRegression
 from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier
 
 
 class BaseModel:
@@ -13,12 +14,19 @@ class BaseModel:
         return '긍정'
 
 
-class TestBaseModel:
-    
-    def test_base_model(self, bn_test_data, accuracy_dict):
-        test_x = bn_test_data['text']
-        test_y = bn_test_data['label']
+@pytest.fixture(scope='session')
+def X_data_emb(bn_train_data, bn_test_data):
+    train_x = bn_train_data['text']
+    test_x = bn_test_data['text']
 
+    bow = CountVectorizer()
+    train_x_emb = bow.fit_transform(train_x)
+    test_x_emb = bow.transform(test_x)
+    return train_x_emb, test_x_emb
+
+class TestModelAccuracy:
+    
+    def test_base_model(self, test_x, test_y, accuracy_dict):
         base_model = BaseModel()
         predict = list(map(base_model.predict, test_x))
         accuracy = sum(predict == test_y) / len(test_y)
@@ -26,17 +34,8 @@ class TestBaseModel:
         print("BaseModel accuracy:", accuracy)
         assert accuracy >= 0.5
 
-    def test_logistic_regression(self, bn_train_data, bn_test_data, accuracy_dict):
-        train_x = bn_train_data['text']
-        train_y = bn_train_data['label']
-
-        test_x = bn_test_data['text']
-        test_y = bn_test_data['label']
-
-        bow = CountVectorizer()
-        train_x_emb = bow.fit_transform(train_x)
-        test_x_emb = bow.transform(test_x)
-
+    def test_logistic_regression(self, train_y, test_y, X_data_emb, accuracy_dict):
+        train_x_emb, test_x_emb = X_data_emb
         lr = LogisticRegression()
         lr.fit(train_x_emb, train_y)
         predict = lr.predict(test_x_emb)
@@ -45,20 +44,20 @@ class TestBaseModel:
         print("LogisticRegression accuracy:", accuracy) 
         assert accuracy >= accuracy_dict['base_model']  # 0.7665
 
-    def test_decison_tree(self, bn_train_data, bn_test_data, accuracy_dict):
-        train_x = bn_train_data['text']
-        train_y = bn_train_data['label']
-
-        test_x = bn_test_data['text']
-        test_y = bn_test_data['label']
-
-        bow = CountVectorizer()
-        train_x_emb = bow.fit_transform(train_x)
-        test_x_emb = bow.transform(test_x)
-
+    def test_decison_tree(self, train_y, test_y, X_data_emb, accuracy_dict):
+        train_x_emb, test_x_emb = X_data_emb
         dt = DecisionTreeClassifier()
         dt.fit(train_x_emb, train_y)
         predict = dt.predict(test_x_emb)
         accuracy = sum(predict == test_y) / len(test_y)
         print("DecisionTree accuracy:", accuracy) 
+        assert accuracy >= accuracy_dict['logistic_regression']
+
+    def test_random_forest(self,  train_y, test_y, X_data_emb, accuracy_dict):
+        train_x_emb, test_x_emb = X_data_emb
+        rf = RandomForestClassifier()
+        rf.fit(train_x_emb, train_y)
+        predict = rf.predict(test_x_emb)
+        accuracy = sum(predict == test_y) / len(test_y)
+        print("RandomForest accuracy:", accuracy) 
         assert accuracy >= accuracy_dict['logistic_regression']
